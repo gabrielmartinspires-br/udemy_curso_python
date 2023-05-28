3a790985921568b0735c08d6e5e01ec8bf89c180
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-28 20:32:42 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,1 @@
-# Ei!
-print('Python')
-print('Explícito', 'é', 'melhor-que-implícito.', sep='-' )
-print('Simples', 'é', 'melhor-que-complexo.', sep='-')
+print('Explícito', 'é', 'melhor " do que implícito')
