ef64a0b6a62254700cfc8cacae95d815b439dece
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-29 05:49:55 -0300

    Create aula11.py

---
+++ aula11.py
@@ -0,0 +1,7 @@
+# 1º    (n + n)     Os parenteses
+# 2º    **          Potenciação
+# 3º    * / // %    Multiplicação, divisão, divisão inteira e módulo
+# 4º    + -         Adição e subtração
+
+conta_1 = 1 + 1  ** 5 + 5
+print(conta_1)
