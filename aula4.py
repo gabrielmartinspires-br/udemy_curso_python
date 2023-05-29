0493bba4778d6dd3ba9204247b47d7eed97e50ad
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-28 22:16:44 -0300

    Update aula4.py

--- aula4.py
+++ aula4.py
@@ -1,2 +1,9 @@
-print(1.1, 10.11) #float
-print(0.0, -1.5) #float
+# print(11) #int
+# print(-11) #int
+# print(0) #int
+# print(1.1, 10.11) #float
+# print(0.0, -1.5) #float
+
+print(type('Gabriel'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
