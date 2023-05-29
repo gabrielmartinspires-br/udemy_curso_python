d2ed54a50388b0a21d6cc89f3b31f997fbc5d84a
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-29 06:21:23 -0300

    Create aula13.py

---
+++ aula13.py
@@ -0,0 +1,8 @@
+nome = 'Gabriel'
+altura = 1.76
+peso = 98
+imc = peso / (altura ** 2)    # IMC = peso / (altura x altura)
+
+print(nome, 'tem', altura, 'de altura')
+print('pesa', peso, 'quilos e seu imc é')
+print(imc)
