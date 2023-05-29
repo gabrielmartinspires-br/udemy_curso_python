fb59ad752a06653d3e7cc99f97c25fbcc8bedd52
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-29 03:21:24 -0300

    Update aula7.py

--- aula7.py
+++ aula7.py
@@ -1,5 +1,11 @@
-nome_completo = 'Gabriel Martins Pires'
-soma_dois_mais_dois = 2 + 2
-int_um = int('1')
-print(int('1'), type(int('1')))
-print(nome_completo, soma_dois_mais_dois)
+# nome_completo = 'Gabriel Martins Pires'
+# soma_dois_mais_dois = 2 + 2
+# int_um = int('1')
+# print(int('1'), type(int('1')))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Gabriel'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
