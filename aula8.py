833ba271279619122b0506ae476aca1272ecfbe8
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-29 03:31:26 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Gabriel'
+sobrenome = 'Martins'
+idade = int(36)
+ano_nascimento = int(1986)
+maior_de_idade = idade >= 18
+altura_metros = float(1.76)
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade:', maior_de_idade)
+print('Altura em metros:', altura_metros)
