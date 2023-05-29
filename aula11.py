e71afab808ed4cd88af03aa8639179802545b189
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-29 05:55:42 -0300

    Update aula11.py

--- aula11.py
+++ aula11.py
@@ -3,5 +3,9 @@
 # 3º    * / // %    Multiplicação, divisão, divisão inteira e módulo
 # 4º    + -         Adição e subtração
 
-conta_1 = 1 + 1  ** 5 + 5
+# conta_1 = (1 + 1)  ** (5 + 5)
+# conta_1 = 1 + 1  ** 5 + 5
+conta_1 = (1 + int(0.5 + 0.5))  ** (5 + 5)
+
+
 print(conta_1)
