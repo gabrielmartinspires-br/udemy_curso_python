55109178678122bc78a2ecd94890f26647b82d24
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-05-31 01:48:41 -0300

    Update aula13.py

--- aula13.py
+++ aula13.py
@@ -3,7 +3,7 @@ altura = 1.76
 peso = 98
 imc = peso / (altura ** 2)    # IMC = peso / (altura x altura)
 
-linha_1 = f'{nome} tem {altura} de altura'
+linha_1 = f'{nome} tem {altura:.2f} de altura'
 
 print(linha_1)
 print('pesa', peso, 'quilos e seu imc é')
