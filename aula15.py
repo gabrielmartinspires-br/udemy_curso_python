8ecf4e7fa90348077fcce4e2efe8af49fd8a81b3
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-02 01:24:36 -0300

    Update aula15.py

--- aula15.py
+++ aula15.py
@@ -1,7 +1,10 @@
 # nome = input('Qual é o seu nome? ')
 # print(f'O seu nome é {nome}')
 
-numero_1 = int(input('Digite um número: '))
-numero_2 = int(input('Digite outro número: '))
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
 
-print(f'a soma dos números é: {numero_1 + numero_2}')
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'a soma dos números é: {int(numero_1) + int(numero_2)}')
