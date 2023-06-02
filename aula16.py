d87c02f7f21816c9094a5b02ef4dc04a7ee382e4
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-02 03:44:59 -0300

    Update aula16.py

--- aula16.py
+++ aula16.py
@@ -7,3 +7,6 @@ if entrada == 'entrar':
     print('Você entrou no sistema')
 elif entrada == 'sair': 
     print('Você saiu do sistema')
+else:
+    print('Você não digitou nem entrar e nem sair.')
+    
