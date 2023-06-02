75a1dec5c5d1780bd2c92054d9f0358735d965a6
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-02 03:41:43 -0300

    Update aula16.py

--- aula16.py
+++ aula16.py
@@ -3,5 +3,7 @@
 
 entrada = input('Você quer "entrar" ou "sair"? ')
 
-print('Você entrou no sistema')
-print('Você saiu do sistema')
+if entrada == 'entrar': 
+    print('Você entrou no sistema')
+elif entrada == 'sair': 
+    print('Você saiu do sistema')
