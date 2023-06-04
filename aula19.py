da3b85534cbeba60738f73952a5031ca24149cf5
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-03 23:47:46 -0300

    Update aula19.py

--- aula19.py
+++ aula19.py
@@ -11,7 +11,10 @@ OP      Significado         Exemplo (True)
 !=      diferente           'a'  != 'b'
 
 """
-maior = 2 > 2
+maior = 2 > 1
 maior_ou_igual = 2 >= 2
 menor = 1 < 2
 menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(maior)
