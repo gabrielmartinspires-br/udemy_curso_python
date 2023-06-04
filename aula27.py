cd2a17548000277643026116f53e33b0bfaa79da
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-04 06:37:41 -0300

    Update aula27.py

--- aula27.py
+++ aula27.py
@@ -10,11 +10,12 @@ de caracteres da str
 """
 
 variavel = 'olá mundo'
-print(variavel[5])
-print(variavel[-4])
-print(variavel[4:])
-print(variavel[4:8])
-print(variavel[0:5])
-print(variavel[1:5])
-print(variavel[-9:-4])
-print(variavel[-8:-2])
+# print(variavel[5])
+# print(variavel[-4])
+# print(variavel[4:])
+# print(variavel[4:8])
+# print(variavel[0:5])
+# print(variavel[1:5])
+# print(variavel[-9:-4])
+# print(variavel[-8:-2])
+print(len(variavel[3]))
