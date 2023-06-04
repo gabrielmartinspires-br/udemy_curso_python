b43227ddc4fd21b9f32ff452a9eb387edc71ec68
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-04 05:49:44 -0300

    Update aula25.py

--- aula25.py
+++ aula25.py
@@ -12,3 +12,4 @@ preco = 1000.95897643
 variavel = '%s, o preço é R$%.2f' % (nome, preco)
 print(variavel)
 print('O hexadecimal de %d é %08x' % (1500, 1500))
+print('O hexadecimal de %d é %08X' % (1500, 1500))
