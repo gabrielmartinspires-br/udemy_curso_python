9145bfbc8e2363a6915d0878ec528307fe2d2a2a
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-04 00:25:59 -0300

    Update aula20.py

--- aula20.py
+++ aula20.py
@@ -1,2 +1,9 @@
 primeiro_valor = input('DIgite um valor: ')
 segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor} é maior ou igual '
+        f'que {segundo_valor}'
+    )
+
