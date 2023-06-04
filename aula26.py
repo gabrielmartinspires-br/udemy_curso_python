dd74f69b91f299e03cd3db657196602c73528163
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-04 03:11:57 -0300

    Create aula26.py

---
+++ aula26.py
@@ -0,0 +1,18 @@
+"""
+Formatação básica de strings
+s - strong
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+
+(Caractere) (<>^) (quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+
+Sinal  + ou -
+Ex.: 0>-100,.1f
+
+Conversion flags - !r !s !a
+"""
