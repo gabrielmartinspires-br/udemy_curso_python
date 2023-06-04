c5c73d3168d0ac018bc18ce2e162e3bfe1e63de7
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-04 01:40:15 -0300

    Update aula21.py

--- aula21.py
+++ aula21.py
@@ -9,4 +9,12 @@
 # usado para representar um não valor
 
 entrada = input('[E]ntrar [S]air: ')
-print(entrada)
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+
+
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print("Sair")
