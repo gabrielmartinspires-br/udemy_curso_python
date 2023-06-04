bc483f2304ed6f5836f2c08a3a8892cfdfda8ad1
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-04 02:26:21 -0300

    Update aula24.py

--- aula24.py
+++ aula24.py
@@ -9,11 +9,11 @@ nome = 'Otávio'
 # print(nome[2])
 # print(nome[-4])
 
-print('vio' in nome)
-print('zero' in nome)
+# print('vio' in nome)
+# print('zero' in nome)
 
-print(10 * '-')
+# print(10 * '-')
 
-print('vio' not in nome)
-print('zero' not in nome)
+# print('vio' not in nome)
+# print('zero' not in nome)
 
