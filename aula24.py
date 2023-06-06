914969210663ae1f16d70f92b0e9cd3894613718
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-06 03:31:12 -0300

    Update aula24.py

--- aula24.py
+++ aula24.py
@@ -6,16 +6,16 @@
 
 nome = 'Otávio'
 
-# print(nome[2])
-# print(nome[-4])
+print(nome[2])
+print(nome[-4])
 
-# print('vio' in nome)
-# print('zero' in nome)
+print('vio' in nome)
+print('zero' in nome)
 
-# print(10 * '-')
+print(10 * '-')
 
-# print('vio' not in nome)
-# print('zero' not in nome)
+print('vio' not in nome)
+print('zero' not in nome)
 
 nome = input('Digite seu nome: ')
 encontrar = input('Digite o que deseja encontrar: ')
