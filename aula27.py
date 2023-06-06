4474238648357e3f76cae9c25a491ff834f10c50
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-06 03:30:05 -0300

    Update aula27.py

--- aula27.py
+++ aula27.py
@@ -15,7 +15,7 @@ variavel = 'olá mundo'
 # print(variavel[4:])
 # print(variavel[4:8])
 # print(variavel[0:5])
-print(variavel[1:5])
+# print(variavel[1:5])
 print(variavel[-9:-4])
 print(variavel[-8:-2])
 print(len(variavel))
