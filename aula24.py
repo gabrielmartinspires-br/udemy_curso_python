a9f51e07fa10090d50bf4cf2adab4d28f25e6ef0
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-06 03:31:30 -0300

    Update aula24.py

--- aula24.py
+++ aula24.py
@@ -6,22 +6,22 @@
 
 nome = 'Otávio'
 
-print(nome[2])
-print(nome[-4])
+# print(nome[2])
+# print(nome[-4])
 
-print('vio' in nome)
-print('zero' in nome)
+# print('vio' in nome)
+# print('zero' in nome)
 
-print(10 * '-')
+# print(10 * '-')
 
-print('vio' not in nome)
-print('zero' not in nome)
+# print('vio' not in nome)
+# print('zero' not in nome)
 
-nome = input('Digite seu nome: ')
-encontrar = input('Digite o que deseja encontrar: ')
+# nome = input('Digite seu nome: ')
+# encontrar = input('Digite o que deseja encontrar: ')
 
-if encontrar in nome:
-    print(f'{encontrar} está em {nome}')
+# if encontrar in nome:
+#     print(f'{encontrar} está em {nome}')
 
-else:
-    print(f'{encontrar} não está em {nome}')
+# else:
+#     print(f'{encontrar} não está em {nome}')
