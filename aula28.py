e1679b366c567230b48490ac904a6ec1302dc16d
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-12 01:45:39 -0300

    Create aula28.py

---
+++ aula28.py
@@ -0,0 +1,11 @@
+nome = input('Digite seu nome: ')
+nome_invertido = (nome[-1:-20:-1])
+quantidade = (len(nome))
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome_invertido}')
+    print(f'Seu nome tem {quantidade} letras')
+else:
+    print('Desculpe, você deixou campos vazios.')
