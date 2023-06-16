ff18217e6570989c70bdc3eb138f3a1acc62ad8d
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-16 01:09:37 -0300

    Update aula29.py

--- aula29.py
+++ aula29.py
@@ -12,8 +12,16 @@ numero_str = input(
     'Vou dobrar o número que você digitar: '
 )
 
-if numero_str.isdigit():
+try:
+    print('STR:', numero_str)
     numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
-else:
+except:
     print('Isso não é um número')
+
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso não é um número')
