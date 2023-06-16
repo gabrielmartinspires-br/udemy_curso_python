0f4e0dd7315976422980535bfc6c1f31ffb48e9a
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-16 01:12:42 -0300

    Update aula29.py

--- aula29.py
+++ aula29.py
@@ -13,7 +13,6 @@ numero_str = input(
 )
 
 try:
-    print('STR:', numero_str)
     numero_float = float(numero_str)
     print('FLOAT:', numero_float)
     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
