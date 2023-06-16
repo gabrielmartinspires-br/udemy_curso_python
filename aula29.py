39be86a05270a34e8d323e1785463583633e8203
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-15 23:03:09 -0300

    Update aula29.py

--- aula29.py
+++ aula29.py
@@ -1,3 +1,9 @@
-print(1234)
-print(5678)
-float('a')
+"""
+Introdução ao try/except
+
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+
+"""
+
+numero = input('Vou dobrar o número que você digitar: ')
