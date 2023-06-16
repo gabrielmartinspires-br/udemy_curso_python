7b1b20358b0df655df736e5866bba50228ba054e
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-15 23:17:14 -0300

    Update aula29.py

--- aula29.py
+++ aula29.py
@@ -8,4 +8,4 @@ except -> ocorreu algum erro ao tentar executar
 
 numero = input('Vou dobrar o número que você digitar: ')
 numero_float = float(numero)
-print(f'O dobro de {numero} é {numero_float * 2}')
+print(f'O dobro de {numero} é {numero_float * 2:.0f}')
