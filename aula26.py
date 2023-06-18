272be6b36ac894c7cd1af6c467444d2f8a15fd3c
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-17 22:55:08 -0300

    Update aula26.py

--- aula26.py
+++ aula26.py
@@ -17,17 +17,3 @@ Ex.: 0>-100,.1f
 Conversion flags - !r !s !a
 """
 variavel = 'ABC'
-# print(f'{variavel: >10}')
-# print(f'{variavel: <10}')
-# print(f'{variavel: ^10}')
-# print(f'{variavel:0^10}')
-# print(f'{1000.4873648123746:,.1f}')
-# print(f'{-1000.4873648123746:-,.1f}')
-# print(f'{1000.4873648123746:+,.1f}')
-# print(f'{1000.4873648123746:0>+10,.1f}')
-# print(f'{1000.4873648123746:0=+10,.1f}')
-# print(f'O hexadecimal de 1500 é {1500:08X}')
-# print(f'O hexadecimal de 1500 é {1500:08x}')
-# print(f'{variavel!r}')
-# print(f'{variavel!s}')
-# print(f'{variavel!a}')
