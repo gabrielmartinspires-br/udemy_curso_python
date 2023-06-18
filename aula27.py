217516929efd3e5c0147510c9a70ef4dbe33e256
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-17 22:55:00 -0300

    Update aula27.py

---
+++ aula27.py
@@ -0,0 +1,26 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: A função len retorna a qtd
+de caracteres da str
+
+"""
+
+variavel = 'olá mundo'
+# print(variavel[5])
+# print(variavel[-4])
+# print(variavel[4:])
+# print(variavel[4:8])
+# print(variavel[0:5])
+# print(variavel[1:5])
+# print(variavel[-9:-4])
+# print(variavel[-8:-2])
+# print(len(variavel))
+# print(len(variavel[3]))
+# print(variavel[0:9:2])
+# print(variavel[-1:-10:-1])
+# print(variavel[-1:-10:-2])
+print(variavel[-1:0:-1])
