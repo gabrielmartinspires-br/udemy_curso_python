642e84739d73a8b2e06925444bdb0e3a5080242b
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-22 04:28:42 -0300

    Update aula28.py

--- aula28.py
+++ aula28.py
@@ -3,4 +3,7 @@ nome_invertido = (nome[-1:-20:-1])
 quantidade = (len(nome))
 idade = input('Digite sua idade: ')
 
-
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome_invertido}')
+ 
