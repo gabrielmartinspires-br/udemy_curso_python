4c69ade0f1e269dcca1473c55fdbc01487352ec1
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2023-06-22 04:28:17 -0300

    Update aula29.py

--- aula29.py
+++ aula29.py
@@ -8,7 +8,16 @@ try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 
 """
+numero_str = input(
+    'Vou dobrar o número que você digitar: '
+)
 
+try:
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
 
 # if numero_str.isdigit():
 #     numero_float = float(numero_str)
